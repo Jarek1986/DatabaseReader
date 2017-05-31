80238b4c904916eccd4cd89617fb90991a1a3043
Jarek <Jarek@Jarek-Komputer>
2017-05-31 22:47:39 +0200

    Change 31.05.2017 B

---
+++ src/main/java/com/mycompany/mavenproject_sql_two/TableFillerOne.java
@@ -0,0 +1,183 @@
+package com.mycompany.mavenproject_sql_two;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class TableFillerOne {
+
+    private SimpleStringProperty a0;
+    private SimpleStringProperty a1;
+    private SimpleStringProperty a2;
+    private SimpleStringProperty a3;
+    private SimpleStringProperty a4;
+    private SimpleStringProperty a5;
+    private SimpleStringProperty a6;
+    private SimpleStringProperty a7;
+    private SimpleStringProperty a8;
+    private SimpleStringProperty a9;
+
+    TableFillerOne(String fName) {
+        a0 = new SimpleStringProperty(fName);
+    }
+
+    TableFillerOne(String fName, String fName1) {
+        a0 = new SimpleStringProperty(fName);
+        a1 = new SimpleStringProperty(fName1);
+    }
+
+    TableFillerOne(String fName, String fName1, String fName2) {
+        a0 = new SimpleStringProperty(fName);
+        a1 = new SimpleStringProperty(fName1);
+        a2 = new SimpleStringProperty(fName2);
+    }
+
+    TableFillerOne(String fName, String fName1, String fName2, String fName3) {
+        a0 = new SimpleStringProperty(fName);
+        a1 = new SimpleStringProperty(fName1);
+        a2 = new SimpleStringProperty(fName2);
+        a3 = new SimpleStringProperty(fName3);
+    }
+
+    TableFillerOne(String fName, String fName1, String fName2, String fName3, String fName4) {
+        a0 = new SimpleStringProperty(fName);
+        a1 = new SimpleStringProperty(fName1);
+        a2 = new SimpleStringProperty(fName2);
+        a3 = new SimpleStringProperty(fName3);
+        a4 = new SimpleStringProperty(fName4);
+    }
+    
+    TableFillerOne(String fName, String fName1, String fName2, String fName3, String fName4, String fName5) {
+        a0 = new SimpleStringProperty(fName);
+        a1 = new SimpleStringProperty(fName1);
+        a2 = new SimpleStringProperty(fName2);
+        a3 = new SimpleStringProperty(fName3);
+        a4 = new SimpleStringProperty(fName4);
+        a5 = new SimpleStringProperty(fName5);
+    }
+    
+    TableFillerOne(String fName, String fName1, String fName2, String fName3, String fName4, String fName5, String fName6) {
+        a0 = new SimpleStringProperty(fName);
+        a1 = new SimpleStringProperty(fName1);
+        a2 = new SimpleStringProperty(fName2);
+        a3 = new SimpleStringProperty(fName3);
+        a4 = new SimpleStringProperty(fName4);
+        a5 = new SimpleStringProperty(fName5);
+        a6 = new SimpleStringProperty(fName6);
+    }
+    TableFillerOne(String fName, String fName1, String fName2, String fName3, String fName4, String fName5, String fName6, String fName7) {
+        a0 = new SimpleStringProperty(fName);
+        a1 = new SimpleStringProperty(fName1);
+        a2 = new SimpleStringProperty(fName2);
+        a3 = new SimpleStringProperty(fName3);
+        a4 = new SimpleStringProperty(fName4);
+        a5 = new SimpleStringProperty(fName5);
+        a6 = new SimpleStringProperty(fName6);        
+        a7 = new SimpleStringProperty(fName7);
+    }
+    
+    TableFillerOne(String fName, String fName1, String fName2, String fName3, String fName4, String fName5, String fName6, String fName7, String fName8) {
+        a0 = new SimpleStringProperty(fName);
+        a1 = new SimpleStringProperty(fName1);
+        a2 = new SimpleStringProperty(fName2);
+        a3 = new SimpleStringProperty(fName3);
+        a4 = new SimpleStringProperty(fName4);
+        a5 = new SimpleStringProperty(fName5);
+        a6 = new SimpleStringProperty(fName6);        
+        a7 = new SimpleStringProperty(fName7);               
+        a8 = new SimpleStringProperty(fName8);
+    }
+    
+    TableFillerOne(String fName, String fName1, String fName2, String fName3, String fName4, String fName5, String fName6, String fName7, String fName8, String fName9) {
+        a0 = new SimpleStringProperty(fName);
+        a1 = new SimpleStringProperty(fName1);
+        a2 = new SimpleStringProperty(fName2);
+        a3 = new SimpleStringProperty(fName3);
+        a4 = new SimpleStringProperty(fName4);
+        a5 = new SimpleStringProperty(fName5);
+        a6 = new SimpleStringProperty(fName6);        
+        a7 = new SimpleStringProperty(fName7);               
+        a8 = new SimpleStringProperty(fName8);                      
+        a9 = new SimpleStringProperty(fName9);
+    }
+
+    public String getA0() {
+        return a0.get();
+    }
+
+    public void setA0(String fName) {
+        a0.set(fName);
+    }
+
+    public String getA1() {
+        return a1.get();
+    }
+
+    public void setA1(String fName1) {
+        a1.set(fName1);
+    }
+
+    public String getA2() {
+        return a2.get();
+    }
+
+    public void setA2(String fName2) {
+        a2.set(fName2);
+    }
+
+    public String getA3() {
+        return a3.get();
+    }
+
+    public void setA3(String fName3) {
+        a3.set(fName3);
+    }
+
+    public String getA4() {
+        return a4.get();
+    }
+
+    public void setA4(String fName4) {
+        a4.set(fName4);
+    }
+
+    public String getA5() {
+        return a5.get();
+    }
+
+    public void setA5(String fName5) {
+        a5.set(fName5);
+    }
+    
+    public String getA6() {
+        return a6.get();
+    }
+
+    public void setA6(String fName6) {
+        a6.set(fName6);
+    }
+    
+     public String getA7() {
+        return a7.get();
+    }
+
+    public void setA7(String fName7) {
+        a7.set(fName7);
+    }
+    
+     public String getA8() {
+        return a8.get();
+    }
+
+    public void setA8(String fName8) {
+        a8.set(fName8);
+    }
+    
+     public String getA9() {
+        return a9.get();
+    }
+
+    public void setA9(String fName9) {
+        a9.set(fName9);
+    }
+
+}
